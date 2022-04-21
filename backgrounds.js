d627123261b21c076d64c2293b729aee4cefde9d
Oliver Christensen <ollie.a.c@outlook.com>
2022-04-20 20:48:15 -0600

    added even less functional javascript to change background that may end up better

--- backgrounds.js
+++ backgrounds.js
@@ -1,21 +1,12 @@
-let btn1 = document.querySelector('#lakeside_button');
-let btn2 = document.querySelector('#forest_button');
-let btn3 = document.querySelector('#seaside_button');
-let btn4 = document.querySelector('#campfire_button');
-let btn5 = document.querySelector('#mountains_button');
-
-btn1.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('photographer.jpg')"
-});
-btn2.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('Forest.jpg')"
-});
-btn3.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('seaside.jpg')"
-});
-btn4.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('campfire.jpg')"
-});
-btn5.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('mountains.jpg')"
-});
+function switchBG(){
+    var img = document.getElementById("theSwitch").value;
+    var page = document.getElementById("pageBody");
+    
+    if(img == 1){
+        page.className = "bgOne";
+    } else if(img == 2){
+        page.className = "bgTwo";
+    } else if(img == 3){
+        page.className = "bgThree";
+    }
+    }
